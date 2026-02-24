3f72b1e7223b4f85d8c7c0b64ccea0aeabd0e036
Mimi Richert <mrichert@uw.edu>
2026-02-24 03:16:35 -0800

    mapbox token

--- js/main1.js
+++ js/main1.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = window.MAPBOX_TOKEN;;
+mapboxgl.accessToken = 'pk.eyJ1IjoibXJpY2hlcnQiLCJhIjoiY21tMGhreHJ4MDB5MTJycHdvMXVybW53eCJ9.d7Y9txTkKEUMLCEmdYvpaw';
 
 const map = new mapboxgl.Map({
   container: 'map',
